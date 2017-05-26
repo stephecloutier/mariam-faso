518737335220ef37177ef45d693c5f3c96971d51
stephecloutier <stephecloutier@gmail.com>
2017-05-26 14:29:59 +0200

    Added font for icons + continue stylized header

--- wordpress/wp-content/themes/mariam-faso/header.php
+++ wordpress/wp-content/themes/mariam-faso/header.php
@@ -10,51 +10,54 @@
     <?php if(is_front_page()): ?>
     <header class="header header--home">
         <div class="header__wrapper">
-            <h1 class="header__title">
-                <a href="index.php" class="header__link">
-                    <img src="wp-content/themes/mariam-faso/assets/images/mariamfaso_logo_alt.svg" alt="<?php bloginfo('name') ?>" width="175" height="auto" class="header__logo"><!--
-                    --><span class="hidden"><?php bloginfo('name') ?></span>
-                </a>
-            </h1>
-    <?php else: ?>
-        <header class="header">
-            <div class="header__wrapper">
+            <div class="header__top">
                 <h1 class="header__title">
                     <a href="index.php" class="header__link">
-                        <img src="wp-content/themes/mariam-faso/assets/images/mariamfaso_logo.svg" alt="<?php bloginfo('name') ?>" width="175" height="auto" class="header__logo"><!--
+                        <img src="wp-content/themes/mariam-faso/assets/images/mariamfaso_logo_alt.svg" alt="<?php bloginfo('name') ?>" width="175" height="auto" class="header__logo"><!--
                         --><span class="hidden"><?php bloginfo('name') ?></span>
                     </a>
                 </h1>
+    <?php else: ?>
+        <header class="header">
+            <div class="header__wrapper">
+                <div class="header__top">
+                    <h1 class="header__title">
+                        <a href="index.php" class="header__link">
+                            <img src="wp-content/themes/mariam-faso/assets/images/mariamfaso_logo.svg" alt="<?php bloginfo('name') ?>" width="175" height="auto" class="header__logo"><!--
+                            --><span class="hidden"><?php bloginfo('name') ?></span>
+                        </a>
+                    </h1>
     <?php endif; ?>
-            <nav class="navigation">
-                <h2 class="hidden"><?= __('Navigation principale', 'mf'); ?></h2>
-                <ul class="navigation__container">
-                    <?php foreach(mf_get_nav_items('main') as $item): ?>
-                    <li class="navigation__item">
-                        <a href="<?= $item->url; ?>" class="navigation__link"><?= $item->label; ?></a>
+                <nav class="navigation">
+                    <h2 class="hidden"><?= __('Navigation principale', 'mf'); ?></h2>
+                    <ul class="navigation__list">
+                        <?php foreach(mf_get_nav_items('main') as $item): ?>
+                        <li class="navigation__item <?php if($item->children){ echo 'navigation__item--parent'; } ?>">
+                            <a href="<?= $item->url; ?>" class="navigation__link"><?= $item->label; ?></a>
 
-                        <?php if($item->children): ?>
-                        <ul class="navigation__sub-container">
+                            <?php if($item->children): ?>
+                            <ul class="navigation__sub-container">
 
-                            <?php foreach($item->children as $sub): ?>
-                            <li class="navigation__sub-item">
-                                <a href="<?= $sub->url; ?>" class="navigation__link"><?= $sub->label; ?></a>
-                            </li>
-                            <?php endforeach; ?>
+                                <?php foreach($item->children as $sub): ?>
+                                <li class="navigation__sub-item">
+                                    <a href="<?= $sub->url; ?>" class="navigation__link"><?= $sub->label; ?></a>
+                                </li>
+                                <?php endforeach; ?>
 
-                        </ul>
-                        <?php endif; ?>
+                            </ul>
+                            <?php endif; ?>
 
-                    </li>
-                <?php endforeach; ?>
-                </ul>
-            </nav>
+                        </li>
+                    <?php endforeach; ?>
+                    </ul>
+                </nav>
+            </div>
 
             <?php if(is_front_page()): ?>
-                <p class="landing__title">
-                    <?= get_field('landingCatchPhrase'); ?>
-                </p>
-                <a href="<?= mf_get_page_url('template-help.php'); ?>" class="landing__button"><?= __('Faire un don', 'mf'); ?></a>
+            <div class="landing__title">
+                <?= get_field('landingCatchPhrase'); ?>
+            </div>
+            <a href="<?= mf_get_page_url('template-help.php'); ?>" class="landing__button"><?= __('Faire un don', 'mf'); ?></a>
             <?php endif; ?>
 
             <?php if(!is_front_page()): ?>
