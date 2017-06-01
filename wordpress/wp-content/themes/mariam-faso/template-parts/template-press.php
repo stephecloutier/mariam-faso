02ef4a4afb3e285911ed768d3a99dea7da50221b
stephecloutier <stephecloutier@gmail.com>
2017-06-01 16:29:29 +0200

    Changed headings structure

--- wordpress/wp-content/themes/mariam-faso/template-parts/template-press.php
+++ wordpress/wp-content/themes/mariam-faso/template-parts/template-press.php
@@ -8,13 +8,13 @@ get_header();
 <main class="main">
     <section class="press">
         <div class="press__introduction introduction">
-            <h2 class="introduction__title"><?= __('Mariam Faso dans la presse', 'mf'); ?></h2>
+            <h1 class="introduction__title"><?= __('Mariam Faso dans la presse', 'mf'); ?></h1>
             <p class="introduction__content"><?= __('À plusieurs reprises, l’ASBL Mariam Faso est mentionnée dans la presse pour ses projets et voyages en Afrique, plus précisément au Burkina Faso et au Maroc.', 'mf'); ?></p>
         </div>
 
         <div class="press__list-wrapper">
             <ul class="press__list">
-                
+
             </ul>
         </div>
 
