de5ea119bc7f1bd82bdd044d0da50fa90f01dfc3
stephecloutier <stephecloutier@gmail.com>
2017-06-12 01:08:39 +0200

    Completed html for Agenda Template

--- wordpress/wp-content/themes/mariam-faso/template-parts/template-agenda.php
+++ wordpress/wp-content/themes/mariam-faso/template-parts/template-agenda.php
@@ -4,36 +4,82 @@
     Template Name: Agenda
 */
 get_header();
-echo 'Agenda';
 ?>
+<?php
+    $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+    $posts = new WP_Query([
+                    'post_type' => 'event',
+                    'posts_per_page' => -1,
+                ]);
 
+
+    $fields = get_fields();
+    $currentDate = date('Y-m-d');
+
+    // to access fields of post type event, I store them in previous or next event array, then I order them.
+    $previousEventsFields = [];
+    $nextEventsFields = [];
+    foreach($posts->posts as $post) {
+        $event = get_fields($post->ID);
+        if($event['eventDate'] >= $currentDate) {
+            $nextEventsFields[] = $event;
+        } else {
+            $previousEventsFields[] = $event;
+        }
+    }
+    usort($previousEventsFields, function($a, $b) {
+        return $b['eventDate'] <=> $a['eventDate'];
+    });
+    usort($nextEventsFields, function($a, $b) {
+        return $a['eventDate'] <=> $b['eventDate'];
+    });
+
+?>
 <main class="main">
-    <div class="main__events events">
-        <h1 class="events__title main-title"><?= __('Agenda', 'mf'); ?></h1>
-
-        <?php
-            $posts = new WP_Query(['showposts' => 3, 'post_type' => 'event']);
-        ?>
-        <?php if($posts->have_posts()) : while($posts->have_posts()) : $posts->the_post(); ?>
-        <?php $fields = get_fields(); ?>
-        <article class="events__event event">
-            <h2 class="event__title"><?= $fields['eventName'] ?></h2>
-            <?php if($fields['eventImg']): ?>
-            <img src="<?= $fields['eventImg']['url']; ?>" alt="<?= mf_get_image_alt('eventImg'); ?>">
-            <?php endif; ?>
+    <div class="agenda">
+        <h1 class="agenda__title main-title"><?= __('Agenda', 'mf'); ?></h1>
+        <p class="main-intro agenda__intro"><?= $fields['agendaIntro']; ?></p>
+        <div class="agenda__events-wrapper agenda__events--next">
+            <span class="agenda__nextEvents"><?= __('Évènements à venir', 'mf'); ?></span>
             <?php
-                $date = $fields['eventDate'];
-                $date = new DateTime($date);
-             ?>
-            <time class="event__time" datetime="<?= $date->format('c'); ?>"><?= $date->format('d'. '/'. 'm'); ?></time>
-
-            <p class="event__desc"><?= $fields['eventShortDesc'] ?></p>
-            <a href="<?php the_permalink(); ?>" class="event__link"><?= __('Voir les informations', 'mf'); ?><span class="hidden"> <?= __('sur', 'mf'); ?> <?= strtolower($fields['eventName']); ?></span></a>
-        </article>
-        <?php endwhile; else: ?>
-        <p class="events__empty"><?= __('Il n’y a pas d’évènements à afficher pour le moment.', 'mf'); ?></p>
+                if($nextEventsFields) :
+                    foreach($nextEventsFields as $nextEvent) :
+            ?>
+            <article class="agenda__event nextEvent">
+                <h2 class="event__title"><?= $nextEvent['eventName'] ?></h2>
+                <?php if($nextEvent['eventImg']): ?>
+                <img src="<?= $nextEvent['eventImg']['url']; ?>" alt="<?= mf_get_image_alt('eventImg'); ?>">
+                <?php endif; ?>
+                <?php $date = new DateTime($nextEvent['eventDate']); ?>
+                <time class="event__time" datetime="<?= $date->format('c'); ?>"><?= $date->format('d'. '/'. 'm'); ?></time>
+                <p class="event__desc"><?= $nextEvent['eventShortDesc'] ?></p>
+                <a href="<?php the_permalink(); ?>" class="event__link"><?= __('Voir les informations', 'mf'); ?><span class="hidden"> <?= __('sur', 'mf'); ?> <?= strtolower($nextEvent['eventName']); ?></span></a>
+            </article>
+        <?php endforeach; else: ?>
+            <p class="events__empty"><?= __('Il n’y a pas d’évènements à afficher pour le moment.', 'mf'); ?></p>
         <?php endif; ?>
-        <a href="<?= mf_get_page_url('template-agenda.php'); ?>" class="events__link--more"><?= __('Voir notre agenda', 'mf'); ?></a>
+        </div>
+
+        <div class="agenda__events-wrapper agenda__events--previous">
+            <span class="agenda__previousEvents"><?= __('Évènements passés', 'mf'); ?></span>
+            <?php
+                if($nextEventsFields) :
+                    foreach($previousEventsFields as $previousEvent) :
+            ?>
+            <article class="agenda__event previousEvent">
+                <h2 class="event__title"><?= $previousEvent['eventName'] ?></h2>
+                <?php if($previousEvent['eventImg']): ?>
+                <img src="<?= $previousEvent['eventImg']['url']; ?>" alt="<?= mf_get_image_alt('eventImg'); ?>">
+                <?php endif; ?>
+                <?php $date = new DateTime($previousEvent['eventDate']); ?>
+                <time class="event__time" datetime="<?= $date->format('c'); ?>"><?= $date->format('d'. '/'. 'm'); ?></time>
+                <p class="event__desc"><?= $previousEvent['eventShortDesc'] ?></p>
+                <a href="<?php the_permalink(); ?>" class="event__link"><?= __('Voir les informations', 'mf'); ?><span class="hidden"> <?= __('sur', 'mf'); ?> <?= strtolower($previousEvent['eventName']); ?></span></a>
+            </article>
+        <?php endforeach; else: ?>
+            <p class="events__empty"><?= __('Il n’y a pas d’évènements précédents pour le moment.', 'mf'); ?></p>
+        <?php endif; ?>
+        </div>
     </div>
 </main>
 
