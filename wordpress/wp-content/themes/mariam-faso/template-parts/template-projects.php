b60444331a5d267f60ce1c3b837ac239eecf058e
stephecloutier <stephecloutier@gmail.com>
2017-06-12 15:25:53 +0200

    Started projects template

--- wordpress/wp-content/themes/mariam-faso/template-parts/template-projects.php
+++ wordpress/wp-content/themes/mariam-faso/template-parts/template-projects.php
@@ -4,17 +4,24 @@
     Template Name: Page des projets
 */
 get_header();
-echo 'All projects';
 $fields = get_fields();
-var_dump($fields);
 ?>
 
 <main class="main">
     <div class="projects">
-        <h1 class="projects__title main-title"></h1>
+        <h1 class="projects__title main-title"><?= __('Nos projets', 'mf') ?></h1>
         <div class="projects__intro main-intro">
-            
+            <?= $fields['projectsIntro']; ?>
         </div>
+        <div class="projects__buttons">
+            <a href="<?= mf_get_page_url('template-help.php'); ?>" class="projects__button"><?= __('Faire un don', 'mf'); ?></a>
+            <a href="<?= mf_get_page_url('template-travel.php'); ?>" class="projects__button"><?= __('Voyager', 'mf'); ?></a>
+            <a href="<?= mf_get_page_url('template-gallery.php'); ?>" class="projects__button"><?= __('Voir toutes les photos', 'mf'); ?></a>
+        </div>
+
+
+
+
     </div>
 
 </main>
