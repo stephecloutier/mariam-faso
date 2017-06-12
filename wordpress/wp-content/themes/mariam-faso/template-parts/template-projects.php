15017f11bd1b5fe182e24008014161ca13ba134e
stephecloutier <stephecloutier@gmail.com>
2017-06-12 14:42:13 +0200

    Started projects template

--- wordpress/wp-content/themes/mariam-faso/template-parts/template-projects.php
+++ wordpress/wp-content/themes/mariam-faso/template-parts/template-projects.php
@@ -5,4 +5,16 @@
 */
 get_header();
 echo 'All projects';
+$fields = get_fields();
+var_dump($fields);
 ?>
+
+<main class="main">
+    <div class="projects">
+        <h1 class="projects__title main-title"></h1>
+        <p class="projects__intro main-intro"></p>
+    </div>
+
+</main>
+
+<?php get_footer(); ?>
