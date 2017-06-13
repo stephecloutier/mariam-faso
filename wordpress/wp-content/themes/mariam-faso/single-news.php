9a00268680c7f2a7fec51f3e7f4d81dbd775797b
stephecloutier <stephecloutier@gmail.com>
2017-06-13 12:18:29 +0200

    Completed single news template

--- wordpress/wp-content/themes/mariam-faso/single-news.php
+++ wordpress/wp-content/themes/mariam-faso/single-news.php
@@ -5,5 +5,24 @@
 */
 
 get_header();
-echo 'Single News';
+$fields = get_fields();
 ?>
+
+<main class="main">
+    <div class="single-news">
+        <h1 class="single-news__title main-title"><?= $fields['newsTitle']; ?></h1>
+        <?php
+            if($fields['newsImg']):
+                $image = $fields['newsImg'];
+        ?>
+        <img width="500" height="auto" src="<?= $image['url']; ?>" alt="<?= mf_get_image_alt($image); ?>" class="single-news__img">
+        <?php endif; ?>
+        <div class="single-news__content main-intro">
+            <?= $fields['newsContent']; ?>
+        </div>
+    </div>
+    <a href="<?= mf_get_page_url('template-news.php') ?>" class="back__link news__back" title="<?= __('Aller sur la page des actualités', 'mf'); ?>"><?= __('Aller à la page des actualités', 'mf'); ?></a>
+    <a href="index.php" class="back__link news__back" title="<?= __('Aller sur la page d’accueil', 'mf'); ?>"><?= __('Retourner sur la page d’accueil', 'mf'); ?></a>
+</main>
+
+<?php get_footer(); ?>
