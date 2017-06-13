9e8c62240f00e3403a9555ccc163187d31075e66
stephecloutier <stephecloutier@gmail.com>
2017-06-13 12:39:55 +0200

    Completed gallery template

---
+++ wordpress/wp-content/themes/mariam-faso/template-parts/template-gallery.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+    Template Name: Galerie photos
+*/
+
+get_header();
+$fields = get_fields();
+?>
+
+<main class="main">
+    <div class="gallery">
+        <h1 class="main-title gallery__title"><?= __('Galerie photos', 'mf'); ?></h1>
+        <div class="main-intro gallery__intro">
+            <?= $fields['galleryIntro']; ?>
+        </div>
+
+        <div class="gallery__photos">
+            <?php $images = $fields['galleryImgs']; ?>
+            <?php foreach($images as $image): ?>
+            <figure class="gallery__figure">
+                <img width="300" height="auto" src="<?= $image['url']; ?>" alt="<?= mf_get_image_alt($image); ?>" class="gallery__img">
+                <?php if($image['description']): ?>
+                <figcaption class="gallery__figcaption">
+                    <?= $image['description']; ?>
+                </figcaption>
+                <?php endif; ?>
+            </figure>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</main>
+
+<?php get_footer(); ?>
