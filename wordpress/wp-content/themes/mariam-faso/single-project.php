4011213e5878749cd249b62a14c86c3b45b53ad7
stephecloutier <stephecloutier@gmail.com>
2017-06-13 11:19:39 +0200

    Completed single-project template

--- wordpress/wp-content/themes/mariam-faso/single-project.php
+++ wordpress/wp-content/themes/mariam-faso/single-project.php
@@ -10,8 +10,53 @@ $fields = get_fields();
     <div class="project">
         <div class="project__landing single-landing">
             <h1 class="single-landing__title project__title"><?= mf_remove_p_tags($fields['projectName']); ?></h1>
+            <p class="single-landing__intro project__intro"><?= $fields['projectDesc']; ?></p>
         </div>
+
+        <article class="project__description">
+            <h2 class="second-title"><?= __('Description du projet', 'mf'); ?></h2>
+            <?php if(have_rows('projectContentRepeater')):
+                while(have_rows('projectContentRepeater')): the_row();
+            ?>
+            <div class="project__contentWrapper">
+                <span class="project__subtitle"><?= get_sub_field('projectContentSubtitle'); ?></span>
+                <div class="project__content">
+                    <?= get_sub_field('projectContent'); ?>
+                </div>
+                <?php
+                    if(get_sub_field('projectContentImgs')):
+                        $images = get_sub_field('projectContentImgs');
+                ?>
+                <div class="project__imgWrapper">
+                    <?php foreach($images as $image): ?>
+                    <img width="400" height="400" src="<?= $image['url']; ?>" alt="<?= mf_get_image_alt($image); ?>" class="project__img">
+                    <?php endforeach; ?>
+                </div>
+                <?php endif; ?>
+            </div>
+            <?php endwhile; endif; ?>
+        </article>
+
+        <div class="project__infos infos">
+            <h2 class="second-title"><?= __('Informations sur le projet', 'mf'); ?></h2>
+            <?php if(have_rows('projectInfosRepeater')):
+                while(have_rows('projectInfosRepeater')): the_row();
+            ?>
+            <div class="project__info info__single">
+                <span class="info__title"><?= get_sub_field('projectInfoName'); ?></span>
+                <div class="info__content">
+                    <?= get_sub_field('projectInfoContent'); ?>
+                </div>
+            </div>
+            <?php endwhile; endif; ?>
+
+            <a href="<?= mf_get_page_url('template-help.php'); ?>" class="infos__button" title="<?= __('Aller sur la page de don', 'mf') ?>"><?= __('Faire un don pour aider ce projet', 'mf'); ?></a>
+        </div>
+
+        <a href="<?= mf_get_page_url('template-projects.php'); ?>" class="project__back" title="<?= __('Aller sur la page des projets', 'mf'); ?>"><?= __('Retourner aux projets', 'mf'); ?></a>
     </div>
+
+    <?php get_template_part('parts/help'); ?>
 
 </main>
 
