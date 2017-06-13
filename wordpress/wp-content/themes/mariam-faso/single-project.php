6239b1fee2628eba6dc7c8ce0b2eadb01c91c59f
stephecloutier <stephecloutier@gmail.com>
2017-06-13 10:28:01 +0200

    Started project and trip template

--- wordpress/wp-content/themes/mariam-faso/single-project.php
+++ wordpress/wp-content/themes/mariam-faso/single-project.php
@@ -3,5 +3,16 @@
     Template Name: Projet
 */
 get_header();
-echo 'Single project';
+$fields = get_fields();
 ?>
+
+<main class="main">
+    <div class="project">
+        <div class="project__landing single-landing">
+            <h1 class="single-landing__title project__title"><?= mf_remove_p_tags($fields['projectName']); ?></h1>
+        </div>
+    </div>
+
+</main>
+
+<?php get_footer(); ?>
