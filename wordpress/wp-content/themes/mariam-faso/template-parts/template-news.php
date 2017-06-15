096f63b11886c021ad671af25047add580037a8c
stephecloutier <stephecloutier@gmail.com>
2017-06-15 19:13:54 +0200

    Put back number of news per page to 5

--- wordpress/wp-content/themes/mariam-faso/template-parts/template-news.php
+++ wordpress/wp-content/themes/mariam-faso/template-parts/template-news.php
@@ -18,7 +18,7 @@ get_header();
     </div>
     <div class="news__wrapper">
         <?php
-            $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+            $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 5;
             $posts = new WP_Query(['showposts' => 1, 'paged' => $paged, 'post_type' => 'news']);
         ?>
         <?php if($posts->have_posts()) : while($posts->have_posts()) : $posts->the_post(); ?>
