d4d81b83b2782cb9f830c78723e6a3059f32a507
stephecloutier <stephecloutier@gmail.com>
2017-06-15 22:35:26 +0200

    Completed news and single-news styles

--- wordpress/wp-content/themes/mariam-faso/single-news.php
+++ wordpress/wp-content/themes/mariam-faso/single-news.php
@@ -12,19 +12,26 @@ $fields = get_fields();
     <ul class="breadcrumb">
     <?php mf_display_breadcrumb(); ?>
     </ul>
-    <div class="news__wrapper">
-        <h1 class="single-news__title main-title"><?= $fields['newsTitle']; ?></h1>
-        <?php
-            if($fields['newsImg']):
-                $image = $fields['newsImg'];
-        ?>
-        <img width="500" height="auto" src="<?= $image['url']; ?>" alt="<?= mf_get_image_alt($image); ?>" class="single-news__img">
-        <?php endif; ?>
-        <div class="single-news__content main-intro">
-            <?= $fields['newsContent']; ?>
+    <div class="news__wrapper single-news">
+        <div class="single-news__heading">
+            <time class="single-news__date" datetime="<?= get_the_date('c'); ?>"><?= get_the_date('d/m/Y') ?></time>
+            <h1 class="single-news__title second-title"><?= $fields['newsTitle']; ?></h1>
         </div>
-        <a href="<?= mf_get_page_url('template-news.php') ?>" class="back__link news__back" title="<?= __('Aller sur la page des actualités', 'mf'); ?>"><?= __('Aller à la page des actualités', 'mf'); ?></a>
-        <a href="index.php" class="back__link news__back" title="<?= __('Aller sur la page d’accueil', 'mf'); ?>"><?= __('Retourner sur la page d’accueil', 'mf'); ?></a>
+
+        <div class="single-news__content">
+            <?php
+                if($fields['newsImg']):
+                    $image = $fields['newsImg'];
+            ?>
+            <img width="500" height="auto" src="<?= $image['url']; ?>" alt="<?= mf_get_image_alt($image); ?>" class="single-news__img">
+            <?php endif; ?>
+            <div class="single-news__content--textual">
+                <?= $fields['newsContent']; ?>
+            </div>
+        </div>
+        <div class="single-news__links">
+            <a href="<?= mf_get_page_url('template-news.php') ?>" class="back__link news__back news__back--news" title="<?= __('Retourner sur la page des actualités', 'mf'); ?>"><?= __('Retourner à la page des actualités', 'mf'); ?></a>
+        </div>
     </div>
 
 </main>
