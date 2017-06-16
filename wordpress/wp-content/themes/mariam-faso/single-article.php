55090ef0e4d6c8ee5176dfcd4342845f7652447a
stephecloutier <stephecloutier@gmail.com>
2017-06-16 10:43:18 +0200

    Stylised single news article page

--- wordpress/wp-content/themes/mariam-faso/single-article.php
+++ wordpress/wp-content/themes/mariam-faso/single-article.php
@@ -10,7 +10,7 @@ get_header();
     <?php mf_display_breadcrumb(); ?>
     </ul>
     <div class="introduction__wrapper">
-        <h1 class="pressArticle__title"><?= $fields['articleTitle']; ?></h1>
+        <h1 class="pressArticle__title second-title"><?= $fields['articleTitle']; ?></h1>
         <?php if($fields['articleIntroduction']): ?>
             <div class="pressArticle__intro main-intro">
                 <?= $fields['articleIntroduction']; ?>
@@ -20,7 +20,7 @@ get_header();
 
     <div class="pressArticle__wrapper">
         <a class="pressArticle__dl" href="<?= $fields['articleImg']['url']; ?>" title="<?= __('Télécharger l’article', 'mf'); ?>"><?= __('Télécharger l’article', 'mf'); ?></a>
-        <img width="300" height="auto" src="<?= wp_get_attachment_image_src($fields['articleImg']['id'], 'full')[0]; ?>" alt="<?= mf_get_image_alt('articleImg'); ?>">
+        <img class="pressArticle__img" width="300" height="auto" src="<?= wp_get_attachment_image_src($fields['articleImg']['id'], 'full')[0]; ?>" alt="<?= mf_get_image_alt('articleImg'); ?>">
         <a class="pressArticle__back back__link" href="<?= mf_get_page_url('template-press.php'); ?>" title="<?= __('Retourner à la liste d’articles', 'mf'); ?>"><?= __('Retourner à la liste d’articles', 'mf'); ?></a>
     </div>
     <div class="moreProjects learn-more">
