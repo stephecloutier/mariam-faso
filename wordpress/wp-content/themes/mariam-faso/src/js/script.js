6b14f7a274211121c9607c615d067dc78bf58a77
stephecloutier <stephecloutier@gmail.com>
2017-06-17 09:32:00 +0200

    Added script to toggle nav menu on responsive

---
+++ wordpress/wp-content/themes/mariam-faso/src/js/script.js
@@ -0,0 +1,20 @@
+console.log("coucou");
+
+let navigationList = document.querySelector(".navigation__list");
+let burgerButton = document.querySelector("#navIconSVG");
+let parentNavItems = document.querySelectorAll(".navigation__item--parent");
+
+const toggleSubMenus = function (oEvent) {
+    oEvent.preventDefault();
+    console.log(oEvent.toElement.parentNode);
+    oEvent.toElement.parentNode.classList.toggle("navigation__item--toggled");
+};
+
+burgerButton.addEventListener("click", () => {
+        navigationList.classList.toggle("navigation__list");
+        navigationList.classList.toggle("navigation__list--toggled");
+});
+parentNavItems.forEach((currentItem) => {
+    console.log(currentItem);
+    currentItem.querySelector(".navigation__link").addEventListener("click", toggleSubMenus);
+});
